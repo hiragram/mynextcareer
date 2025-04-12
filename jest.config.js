c123c5cf99360f7bbede8998646b1a73008a5f8d
Yuya Hirayama <hiragram@gmail.com>
2025-04-13 08:41:11 +0900

    テスト設定を修正

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1'
   },
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: [
-    '/node_modules/(?!next/link)'
-  ],
 };
